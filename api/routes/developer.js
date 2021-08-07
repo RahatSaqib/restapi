3458cb6efb364de808e993ea23c76a999d7c360c
Rahat Saqib <rahatsaqib78@gmail.com>
2021-08-07 20:32:17 +0600

    THIRD COMMIT

--- api/routes/developer.js
+++ api/routes/developer.js
@@ -11,12 +11,13 @@ var req = require('request');
 router.get('/',(req,res,next)=>{
     res.set('Content-Type', 'application/json');
     res.set('URL', 'https://localhost:3000/developer');
-    res.set('method', 'POST');
+    res.set('method', 'GET');
     
         developer.find().then(result=>{
             res.status(200).json({
                 DeveloperData:result
             })
+        })
             .catch(err=>{
                 console.log(err);
                 res.status(500).json({
@@ -27,7 +28,7 @@ router.get('/',(req,res,next)=>{
     
        
     
-})
+
 
 router.post('/', async(req,res,next)=>{
 
