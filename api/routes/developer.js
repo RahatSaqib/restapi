661edbed70e9ddaf02dd3f12d6ea621c8a9d6687
Rahat Saqib <rahatsaqib78@gmail.com>
2021-08-07 18:57:23 +0600

    first commit

--- api/routes/developer.js
+++ api/routes/developer.js
@@ -4,11 +4,21 @@ const developer=require('../model/devdata');
 const mongoose=require('mongoose')
 
 router.get('/',(req,res,next)=>{
-    res.status(200).json({
-        message :'GET'
-    }
+    
+        developer.find().then(result=>{
+            res.status(200).json({
+                DeveloperData:result
+            })
+            .catch(err=>{
+                console.log(err);
+                res.status(500).json({
+                    error:err
+                })
+            })
+        })
+    
        
-    )
+    
 })
 router.post('/',(req,res,next)=>{
    const developerr=new developer({
